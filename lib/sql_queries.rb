46c513a45c21da8f5ad2f549bf7a521ac188808a
Audrey Jones <audrey.eliz.jones@gmail.com>
2019-03-20 18:37:02 +0000

    Done.

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -24,7 +24,11 @@ ON users.id = pledges.user_id GROUP BY(users.name) ORDER BY(users.name)"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-"Write your SQL query Here"
+  # projects.funding_goal = SUM(pledges.amount)
+  # Expected: [["My book on SQL", 0], ["The next Inna-Gadda-Davida", 142]]
+"SELECT projects.title, (projects.funding_goal - SUM(pledges.amount)) FROM projects
+INNER JOIN pledges
+ON projects.id = pledges.project_id GROUP BY(projects.funding_goal) ORDER BY(projects.funding_goal) DESC"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
